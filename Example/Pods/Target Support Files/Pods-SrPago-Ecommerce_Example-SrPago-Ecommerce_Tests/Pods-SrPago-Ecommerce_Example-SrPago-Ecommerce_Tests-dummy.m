e55df04d0a724040dfecc44d0fdf0c0acf2177c4
Luis Ferandno Bustos Ramirez <fbustosr@srpago.com>
2020-11-26 21:53:33 -0600

    ecommerce-sdk was updated, removing uiwebview, unusable code was removed

--- Example/Pods/Target Support Files/Pods-SrPago-Ecommerce_Example-SrPago-Ecommerce_Tests/Pods-SrPago-Ecommerce_Example-SrPago-Ecommerce_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SrPago_Ecommerce_Example_SrPago_Ecommerce_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_SrPago_Ecommerce_Example_SrPago_Ecommerce_Tests
-@end
